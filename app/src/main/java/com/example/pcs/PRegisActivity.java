3779cf541ca11a243588ef8c3c9c1bb89bc386de
thanakorn <thanakorn.kua@ku.th>
2022-02-11 03:16:44 +0700

    edit

--- app/src/main/java/com/example/pcs/PRegisActivity.java
+++ app/src/main/java/com/example/pcs/PRegisActivity.java
@@ -24,6 +24,8 @@ public class PRegisActivity extends AppCompatActivity {
     TextInputLayout UserID, Password, ConPass, Email, NationalIDCard;
     ImageView TitleText, Back;
     Button Next, Patient, Doctor;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +42,14 @@ public class PRegisActivity extends AppCompatActivity {
         TitleText         = findViewById(R.id.titletext);
         Back              = findViewById(R.id.Back);
 
-        int x = 1;
+        /*int x = 1;
         if(x==1){
             UserID.getEditText().setText("Yutthana");
         } else {
 
-        }
+        }*/
+
+
 
         Patient.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +69,10 @@ public class PRegisActivity extends AppCompatActivity {
         });
     }
     public void CallNextRegister(View view){
+
+        if (!validateUserID() | !validatePassword() | !validateEmail() | !validateNationalIDCard()) {
+            return;
+        }
 
         Intent call = new Intent(getApplicationContext(), PRegis2Activity.class);
 
@@ -98,6 +106,85 @@ public class PRegisActivity extends AppCompatActivity {
             startActivity(call, options.toBundle());
         } else {
             startActivity(call);
+        }
+
+    }
+    private boolean validateUserID() {
+        String val = UserID.getEditText().getText().toString().trim();
+        String checkspaces = "Aw{1,20}z";
+
+        if (val.isEmpty()) {
+            UserID.setError("Field can not be empty");
+            return false;
+        } else if (val.length() > 20) {
+            UserID.setError("Username is too large!");
+            return false;
+        } else if (!val.matches(checkspaces)) {
+            UserID.setError("No White spaces are allowed!");
+            return false;
+        } else {
+            UserID.setError(null);
+            UserID.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String val = Password.getEditText().getText().toString().trim();
+        String checkPassword = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                //"(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        if (val.isEmpty()) {
+            Password.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkPassword)) {
+            Password.setError("Password should contain 4 characters!");
+            return false;
+        } else {
+            Password.setError(null);
+            Password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+
+    private boolean validateEmail() {
+        String val = Email.getEditText().getText().toString().trim();
+        String checkEmail = "[a-zA-Z0-9._-]+@[a-z]+.+[a-z]+";
+
+        if (val.isEmpty()) {
+            Email.setError("Field can not be empty");
+            return false;
+        } else if (!val.matches(checkEmail)) {
+            Email.setError("Invalid Email!");
+            return false;
+        } else {
+            Email.setError(null);
+            Email.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateNationalIDCard() {
+        String val = NationalIDCard.getEditText().getText().toString().trim();
+        if (val.isEmpty()) {
+            NationalIDCard.setError("Field can not be empty");
+            return false;
+        } else if (val.length() > 14) {
+            NationalIDCard.setError("National ID Card is too large!");
+            return false;
+        } else {
+            NationalIDCard.setError(null);
+            NationalIDCard.setErrorEnabled(false);
+            return true;
         }
     }
 }
