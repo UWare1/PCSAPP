d046a7e9fa68e197aff6e4f29974d9ecae13eb94
Yutthana <yutthana.pi@ku.th>
2022-02-15 22:26:16 +0700

    Project Update

--- app/src/main/java/com/example/pcs/PFPassActivity.java
+++ app/src/main/java/com/example/pcs/PFPassActivity.java
@@ -2,16 +2,24 @@ package com.example.pcs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class PFPassActivity extends AppCompatActivity {
 
-    EditText RecoverEmail;
+    ImageView TitleText;
+    TextInputLayout RecoverEmail;
     Button Recover, Patient, Doctor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,7 @@ public class PFPassActivity extends AppCompatActivity {
         Recover      = findViewById(R.id.Recover);
         Patient   = findViewById(R.id.Patient);
         Doctor    = findViewById(R.id.Doctor);
+        TitleText         = findViewById(R.id.titletext);
 
         Patient.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +48,31 @@ public class PFPassActivity extends AppCompatActivity {
                 finish();
             }
         });
+        Recover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Rec = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(Rec);
+                finish();
+            }
+        });
+    }
+    public void CallBack(View view) {
+
+        Intent call = new Intent(getApplicationContext(), MainActivity.class);
+
+        Pair[] pairs = new Pair[3];
+
+        pairs[0] = new Pair<View, String>(TitleText, "transition_title");
+        pairs[1] = new Pair<View, String>(Patient, "transition_patient_btn");
+        pairs[2] = new Pair<View, String>(Doctor, "transition_doctor_btn");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(PFPassActivity.this,pairs);
+            startActivity(call, options.toBundle());
+        } else {
+            startActivity(call);
+        }
     }
 }
 
